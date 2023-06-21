9621b57924cc85c81d239ca4fdfd7cff9e00d3eb
Lilli Peura <lilli.peura@aalto.fi>
2023-06-21 12:22:34 +0300

    added collaborated- query and modified convert.py file so that it takes an input and only executes the selected query

--- convert.py
+++ convert.py
@@ -9,6 +9,19 @@ def execute_query(store, write_file, query, name):
     write_file.write(response.text)
 
 
+
+filename = input("Enter file name: ") #executing only the selected query 
+f_path = "queries/" + filename + ".SPARQL"
+q_name = filename.split(".")[0]
+q_file = open(f_path, "r")
+query = q_file.read()
+q_file.close()
+w_file = open("ttl/" + q_name + ".ttl", "w+")
+execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, filename)
+w_file.close()
+
+
+"""
 q_dir ="queries/"
 files = os.listdir(q_dir)
 
@@ -21,3 +34,4 @@ for f_name in files:
     w_file = open("ttl/" + q_name + ".ttl", "w+")
     execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, f_name)
     w_file.close()
+"""
