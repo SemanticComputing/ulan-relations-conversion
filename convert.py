e21a2d29bc15ef2efbca12db12f7013e6c5dfdae
Lilli Peura <lilli.peura@aalto.fi>
2023-06-21 15:43:49 +0300

    added option for executing all the queries

--- convert.py
+++ convert.py
@@ -9,29 +9,30 @@ def execute_query(store, write_file, query, name):
     write_file.write(response.text)
 
 
+filename = input("Enter file name or type 'a' for executing all the queries ") 
 
-filename = input("Enter file name: ") #executing only the selected query 
-f_path = "queries/" + filename + ".SPARQL"
-q_name = filename.split(".")[0]
-q_file = open(f_path, "r")
-query = q_file.read()
-q_file.close()
-w_file = open("ttl/" + q_name + ".ttl", "w+")
-execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, filename)
-w_file.close()
+if filename == "a": #executing all the queries
+    q_dir ="queries/"
+    files = os.listdir(q_dir)
 
+    for f_name in files:
+        f_path = 'queries/' + f_name
+        q_name = f_name.split(".")[0]
+        q_file = open(f_path, "r")
+        query = q_file.read()
+        q_file.close()
+        w_file = open("ttl/" + q_name + ".ttl", "w+")
+        execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, f_name)
+        w_file.close()
 
-"""
-q_dir ="queries/"
-files = os.listdir(q_dir)
-
-for f_name in files:
-    f_path = 'queries/' + f_name
-    q_name = f_name.split(".")[0]
+else: #executing only the selected query 
+    f_path = "queries/" + filename + ".SPARQL"
+    q_name = filename.split(".")[0]
     q_file = open(f_path, "r")
     query = q_file.read()
     q_file.close()
     w_file = open("ttl/" + q_name + ".ttl", "w+")
-    execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, f_name)
+    execute_query("http://vocab.getty.edu/sparql.ttl", w_file, query, filename)
     w_file.close()
-"""
+
+
